b0d8d9f03a8e72a235214abd9042c98075924288
arakhmatullin <arakhmatullin@cloudally.com>
2019-09-21 19:24:52 +0300

    commmit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post=new Post(); // ctrl alt v
+        System.out.println(post.id);
+        System.out.println(post.removed);
+        post.id =1;
+        System.out.println(post.id);
+        char newLine='\n';
+        char random=`\u1232`;
+        String name ="IT Academy"; // только строки
+    }
+}
